39b7f5b5b6cac8848ede500b6e8b82c86f29d2c9
unknown <brianpariz@hotmail.com>
2019-05-25 23:06:39 -0300

    continuo el desarrollo de la v2

--- Sala-de-Juegos-v2/src/app/app.module.ts
+++ Sala-de-Juegos-v2/src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrincipalComponent } from './componentes/principal/principal.component';
+import { CabeceraComponent } from './componentes/cabecera/cabecera.component';
+import { PieComponent } from './componentes/pie/pie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrincipalComponent,
+    CabeceraComponent,
+    PieComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
