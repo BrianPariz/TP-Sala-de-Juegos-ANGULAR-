24068fabee637731e2b385628d1c59246b96f12a
BrianPariz <brianpariz@hotmail.com>
2019-05-29 01:54:57 -0300

    algo

---
+++ Sala-de-Juegos-v3/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PrincipalComponent } from './componentes/principal/principal.component';
+import { CabeceraComponent } from './componentes/cabecera/cabecera.component';
+import { PieComponent } from './componentes/pie/pie.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PrincipalComponent,
+    CabeceraComponent,
+    PieComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
