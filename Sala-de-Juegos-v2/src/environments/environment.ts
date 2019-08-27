af1b86e1734546c0272eaf6b350db45dd66bfedb
Brian Pariz <brianpariz@hotmail.com>
2019-08-27 11:12:41 -0300

    Update environment.ts

--- Sala-de-Juegos-v2/src/environments/environment.ts
+++ Sala-de-Juegos-v2/src/environments/environment.ts
@@ -5,13 +5,7 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyAKm2lkM_4Yn0a4oEy5I6xM5VQMgbvx2hw",
-    authDomain: "sala-de-juegos-546f1.firebaseapp.com",
-    databaseURL: "https://sala-de-juegos-546f1.firebaseio.com",
-    projectId: "sala-de-juegos-546f1",
-    storageBucket: "sala-de-juegos-546f1.appspot.com",
-    messagingSenderId: "204378112591",
-    appId: "1:204378112591:web:03fce0298d9fcb42"
+    
   }
 };
 
